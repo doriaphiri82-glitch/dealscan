2f3800613ab7e85d6a9cef3eb94ae51092f7241b
doriaphiri82-glitch <doriaphiri82@gmail.com>
2026-09-04 14:42:46 +0200

    polish: strengthen DealScan landing workflow

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-
 import Navbar from '@/components/Navbar'
 import ScrollReveal from '@/components/ScrollReveal'
 import Hero from '@/components/Hero'
+import WorkflowRibbon from '@/components/WorkflowRibbon'
 import Problem from '@/components/Problem'
 import Audience from '@/components/Audience'
 import Solution from '@/components/Solution'
@@ -12,24 +12,4 @@ import Pricing from '@/components/Pricing'
 import FAQ from '@/components/FAQ'
 import FinalCTA from '@/components/FinalCTA'
 import Footer from '@/components/Footer'
-
-export default function Home() {
-  return (
-    <>
-      <ScrollReveal />
-      <Navbar />
-      <main id="main-content" className="min-h-screen bg-[#f7f9f7] text-[#15211b] selection:bg-[#dceee3] selection:text-[#153025]">
-        <Hero />
-        <Problem />
-        <Audience />
-        <Solution />
-        <HowItWorks />
-        <SampleDeal />
-        <Pricing />
-        <FAQ />
-        <FinalCTA />
-      </main>
-      <Footer />
-    </>
-  )
-}
+export default function Home(){return <><ScrollReveal/><Navbar/><main id="main-content" className="min-h-screen bg-[#f7f9f7] text-[#15211b] selection:bg-[#dceee3] selection:text-[#153025]"><Hero/><WorkflowRibbon/><Problem/><Audience/><Solution/><HowItWorks/><SampleDeal/><Pricing/><FAQ/><FinalCTA/></main><Footer/></>}
